9d91fc2191b659eefc8904c65f8601dd5471182d
Antowa-qq <anton-vinsss@mail.ru>
2020-05-15 20:06:11 +0300

    feat: basic configurations

---
+++ movie-search/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    env: {
+        browser: true,
+        es6: true,
+        node: true
+    },
+    parser: "babel-eslint",
+    extends: "airbnb-base",
+    globals: {
+        Atomics: "readonly",
+        SharedArrayBuffer: "readonly"
+    },
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: "module"
+    },
+    rules: {
+        "class-methods-use-this": "off",
+        "guard-for-in": "off",
+        "no-loop-func": "off",
+        "import/prefer-default-export": "off",
+        "no-use-before-define": "off",
+        "no-restricted-syntax": "off"
+    }
+};
