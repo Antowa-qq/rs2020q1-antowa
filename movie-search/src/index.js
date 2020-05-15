3233699cdcc443ac5fef44607f43ddbab0371a69
Antowa-qq <anton-vinsss@mail.ru>
2020-05-15 20:19:15 +0300

    feat: add start file index.js

---
+++ movie-search/src/index.js
@@ -0,0 +1,6 @@
+import Controller from './modules/Controller';
+import './style.css';
+import '../node_modules/swiper/css/swiper.min.css';
+
+const controller = new Controller();
+controller.init();
